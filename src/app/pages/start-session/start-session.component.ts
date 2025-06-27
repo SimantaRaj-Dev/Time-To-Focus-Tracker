741783fccbd691ca3575bfba358ca0ea25519540
Simanta Raj Deb <simanta.deb@zetwerk.com>
2025-06-27 20:26:48 +0530

    Basic setup and routes done.

---
+++ src/app/pages/start-session/start-session.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-session',
+  standalone: true,
+  imports: [],
+  templateUrl: './start-session.component.html',
+  styleUrl: './start-session.component.scss'
+})
+export class StartSessionComponent {
+
+}
