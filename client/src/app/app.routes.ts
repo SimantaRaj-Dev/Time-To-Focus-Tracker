6ae6ad0a210d09dee8d33c81ca4b841fae622522
Simanta-Developer <simantaraj45@gmail.com>
2025-07-25 02:40:22 +0530

    Routes changed/hidden + Fixes done for sidebar routing.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const appRoutes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
   { path: 'history', loadComponent: () => import('./pages/history/history.component').then(m => m.HistoryComponent) },
-  { path: 'insights', loadComponent: () => import('./pages/insight/insight.component').then(m => m.InsightComponent) },
+  { path: 'insights/:id', loadComponent: () => import('./pages/insight/insight.component').then(m => m.InsightComponent) },
   { path: '**', redirectTo: '' }
 ];
 
